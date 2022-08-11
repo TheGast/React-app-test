a2001f9f501a081c431a49625028dc1838c3f8d7
TheGast <wotthegast@yandex.ru>
2022-08-11 15:13:34 +0300

    3

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,11 @@
 import Promo from "../Promo/Promo";
+import Popular from "../Popular/Popular";
 
 function Main() {
   return (
     <div className="main">
       <Promo/>
+      <Popular/>
     </div>
   );
 }
---
+++ src/Components/Popular/Popular.js
@@ -0,0 +1,36 @@
+import './Popular.scss';
+
+function Popular() {
+  return (
+    <section className="popular">
+      <div className="container">
+        <h2 className="popular__title">Популярные категории</h2>
+        <div className="popular__content">
+          <div className="categories-item gridDS">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item gridDS">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+          <div className="categories-item">
+            <h4 className="categories-item__title">Лен и полулен</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Popular;
